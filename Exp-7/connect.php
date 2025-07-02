5f742e04e4507758eed5cb75458168e5c68d05e7
Madhubala-123 <166892780+Madhubala-123@users.noreply.github.com>
2025-07-02 23:56:38 +0530

    connect.php

---
+++ Exp-7/connect.php
@@ -0,0 +1,12 @@
+<?php
+$host = "localhost";
+$user = "root";               // Your MySQL username
+$password = "";               // Your MySQL password
+$database = "beauty_booking"; // Database name
+
+$conn = new mysqli($host, $user, $password, $database);
+
+if ($conn->connect_error) {
+  die("Database Connection Failed: " . $conn->connect_error);
+}
+?>
